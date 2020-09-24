8d4a1394ebcd030b19962231254d96f5ad347e8b
andycaabrera <andrescabrear8199@gmail.com>
2020-09-23 22:46:20 -0300

    Delete and Update de una bici

--- red_bicicletas/controllers/bicicleta.js
+++ red_bicicletas/controllers/bicicleta.js
@@ -15,3 +15,27 @@ exports.bicicleta_create_post = function(req,res){
 
     res.redirect('/bicicletas');
 }
+
+exports.bicicleta_delete_post = function(req, res){
+    Bicicleta.removeById(req.body.id);
+
+    res.redirect('/bicicletas');
+}
+
+exports.bicicleta_update_get = function(req, res){
+    var bici = Bicicleta.findById(req.params.id);
+    
+    res.render('bicicletas/update',{bici})
+}
+
+exports.bicicleta_update_post = function(req, res){
+    var bici = Bicicleta.findById(req.params.id);
+
+    bici.id = req.body.id;
+    bici.color = req.body.color;
+    bici.modelo = req.body.modelo;
+    bici.ubicacion = [req.body.lat, req.body.lng];
+
+    res.redirect('/bicicletas');
+
+}
--- red_bicicletas/models/bicicleta.js
+++ red_bicicletas/models/bicicleta.js
@@ -14,6 +14,23 @@ Bicicleta.add = function(unaBici){
     Bicicleta.allBicis.push(unaBici);
 }
 
+Bicicleta.findById = function(unaBiciId){
+    var unaBici = Bicicleta.allBicis.find(x => x.id == unaBiciId);
+    if(unaBici)
+        return unaBici
+    else
+        throw new Error(`No existe una Bicicleta con el id ${unaBiciId}`);
+}
+
+Bicicleta.removeById = function(unaBiciId){
+    for(var i = 0; i < Bicicleta.allBicis.length; i++){
+        if(Bicicleta.allBicis[i].id == unaBiciId){
+            Bicicleta.allBicis.splice(i, 1);
+            break;
+        }
+    }
+}
+
 var a = new Bicicleta(1, 'rojo', 'urbana', [-34.6012424,-58.3861497]);
 var b = new Bicicleta(2, 'azul', 'urbana', [-34.6012424,-58.380864]);
 
