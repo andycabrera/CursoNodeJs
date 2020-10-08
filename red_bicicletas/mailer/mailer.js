3a023d12184cba1923dd3abd2b4a22e13100cf2a
andycaabrera <andrescabrear8199@gmail.com>
2020-10-07 21:17:21 -0300

    agregamos sendgrid

--- red_bicicletas/mailer/mailer.js
+++ red_bicicletas/mailer/mailer.js
@@ -1,12 +1,34 @@
 const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
 
-const mailConfig = {
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'khalid.daniel@ethereal.email',
-        pass: 'fRXEahvr4M7AgAg7jB'
+let mailConfig;
+
+if(process.env.NODE_ENV === 'producction'){
+    const options = {
+        auth: {
+            api_key: process.env.SENDGRID_API_KEY
+        }
     }
-};
+    mailConfig = sgTransport(options);
+}else {
+    if(process.env.NODE_ENV === 'staging'){
+        console.log('XXXXXXXXXXXXXXX');
+        const options = {
+            auth: {
+                epi_key: process.env.SENDGRID_API_KEY
+            }
+        }
+        mailConfig = sgTransport(options);
+    }else{
+        mailConfig = {
+            host: 'smtp.ethereal.email',
+            port:587,
+            auth: {
+                user: process.env.ethereal_user,
+                pass: process.env.ethereal_pwd
+            }
+        };
+    }
+}
 
 module.exports = nodemailer.createTransport(mailConfig);
