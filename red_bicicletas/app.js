e139bf6b3f947194c1723e5b1de7ba23bfeddf72
andycaabrera <andrescabrear8199@gmail.com>
2020-10-07 22:52:36 -0300

    Agregamos politica de privacidad

--- red_bicicletas/app.js
+++ red_bicicletas/app.js
@@ -137,6 +137,10 @@ app.use('/api/auth', authAPIRouter);
 app.use('/api/bicicletas',validarUsuario, bicicletasAPIRouter);
 app.use('/api/usuarios', usuariosAPIRouter);
 
+app.use('./privacy_policy', function(req, res){
+  res.sendFile('public/policy_privacy.html');
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
